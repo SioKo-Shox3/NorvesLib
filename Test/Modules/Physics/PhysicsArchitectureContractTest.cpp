9eec8fb31b04b820132b81da64ea20ca18017daa
Sioko-Shox3 <>
2026-09-17 09:06:26 +0900

    ABI契約を現行snapshotへ同期する

--- Test/Modules/Physics/PhysicsArchitectureContractTest.cpp
+++ Test/Modules/Physics/PhysicsArchitectureContractTest.cpp
@@ -77,9 +77,9 @@ namespace
     static_assert(!std::is_same_v<TArrayElement<decltype(Rendering::SceneProxy::LightProxies)>, Entity>);
     static_assert(std::is_same_v<TArrayElement<decltype(Rendering::SceneProxy::SkinnedMeshProxies)>, Rendering::SkinnedMeshProxy>);
     static_assert(std::is_same_v<TArrayElement<decltype(Rendering::SkinnedMeshProxy::BonePalette)>, Math::Matrix4x4>);
-    static_assert(sizeof(Rendering::FramePacket) == 720);
+    static_assert(sizeof(Rendering::FramePacket) == 768);
     static_assert(alignof(Rendering::FramePacket) == 8);
-    static_assert(sizeof(Rendering::SceneProxy) == 320);
+    static_assert(sizeof(Rendering::SceneProxy) == 352);
     static_assert(alignof(Rendering::SceneProxy) == 8);
 
     bool IsAsciiIdentifierCharacter(char character)
